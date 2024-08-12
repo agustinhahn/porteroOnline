b84de3ab90a22461997804e0d1d721faff95f3bc
Agustin Hahn <125411798+agustinhahn@users.noreply.github.com>
2024-08-11 21:58:54 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./app.css";
 function App() {
   return (
     <div className="contenedorGral">
-      <h1 className="tituloGral">EDIFICIO ALEM 1946</h1>
+      <h1 className="tituloGral">ALEM 1946</h1>
       <div className="contenedorBotonesGral">
         {data.map((d, index) => {
           return <Departamento key={index} depto={d.departamento} />;
